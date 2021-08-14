e793adaae8b83cfdc6b5802314bd2d2dae9b42bb
EV3RETH <ev3reth@gmail.com>
2021-08-13 20:01:21 -0700

    last adjustment

--- packages/next-frontend/pages/index.tsx
+++ packages/next-frontend/pages/index.tsx
@@ -9,9 +9,6 @@ import Waves from '../components/waves'
 import titleColor from '../public/images/HypeDAO-color.png'
 import titlePlain from '../public/images/HypeDAO-plain.png'
 import logo from '../public/images/HYPEDAO-logo-image.png'
-import { useEffect } from 'react'
-import dynamic from "next/dynamic"
-import useWindowDimensions from '../hooks/windowDimensions'
 
 
 interface HomeProps {
@@ -20,21 +17,13 @@ interface HomeProps {
 export default function Home({ bgIsLoaded }: HomeProps) {
 	const [wavesOn, setWavesOn] = useState(false)
 
-	const { height } = useWindowDimensions()
-
-	useEffect(() => {
-		//setting the inner height to determine mobiles true height inside all the controls
-		const vh = height * 0.01;
-		// document.documentElement.style.setProperty('--vh', `${vh}px`);
-	}, [height])
-
 	const toggleWaves = () => setWavesOn(prev => !prev)
 
 	const wavesBgColor = "rgba(0, 0, 0, 0.05)"
 
 	return (
 		<Layout >
-			<main className={styles.homeContainer} style={{ height: `${height}px` }}>
+			<main className={styles.homeContainer}>
 
 				<button className={classNames(styles.homeTitle, utilStyles.noStyle, utilStyles.titleXl, { [styles.imageLoaded]: bgIsLoaded })} onClick={toggleWaves}>
 					<div className={classNames(styles.titleColor, { [styles.isActive]: wavesOn, [styles.imageLoaded]: bgIsLoaded })} >
