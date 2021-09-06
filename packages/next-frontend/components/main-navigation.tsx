26b59f499aa946dd76e16f0eaa491fe10adda4db
singulart <serpian@gmail.com>
2021-09-06 10:19:30 +0300

    PR comments addressed

--- packages/next-frontend/components/main-navigation.tsx
+++ packages/next-frontend/components/main-navigation.tsx
@@ -10,7 +10,7 @@ import HypeRegistrationButton from "./hype-registration-button";
 export const PATHNAMES = {
 	HOME: "/",
 	ABOUT: "/about",
-	POSTS_DEMO: "/posts",
+	POSTS: "/posts",
 	BOUNTIES: {
 		MONTHLY: "/bounties/monthly"
 	}
@@ -65,8 +65,8 @@ export default function MainNavigation() {
 					<Link href={PATHNAMES.BOUNTIES.MONTHLY}>
 						<a className={classNames(styles.link, { [styles.selected]: pathname === PATHNAMES.BOUNTIES.MONTHLY })}>Bounties</a>
 					</Link>
-					<Link href={PATHNAMES.POSTS_DEMO}>
-						<a className={classNames(styles.link, { [styles.selected]: pathname === PATHNAMES.POSTS_DEMO })}>Posts Demo</a>
+					<Link href={PATHNAMES.POSTS}>
+						<a className={classNames(styles.link, { [styles.selected]: pathname === PATHNAMES.POSTS })}>Posts</a>
 					</Link>
 				</div>
 			</nav>
--- packages/next-frontend/pages/posts.tsx
+++ packages/next-frontend/pages/posts.tsx
@@ -7,29 +7,20 @@ export default function Posts() {
 	return (
 		<Layout>
 			<main>
-				<h1 className={utilStyles.title}>Discourse Integration Demo</h1>
-				<div className={utilStyles.scrim}>
-					<h2 style={{ marginTop: "0" }}>Solution highlights</h2>
-
-					<p>This is second attempt to deliver Discourse integration functionality. No Open API used this time. Everything is super simple: HTTP GET request is done via Axios, response is put to component internal state and then rendered.</p>
+				<h1 className={utilStyles.title}>Forum Highlights</h1>
 				<Table>
 					<tbody>
 					<tr>
-						<td><DiscoursePost postId="14215"/></td>
+						<td><DiscoursePost postId="14993"/></td>
 					</tr>
 					<tr>
-						<td><DiscoursePost postId="14212"/></td>
+						<td><DiscoursePost postId="13287"/></td>
 					</tr>
 					<tr>
-						<td><DiscoursePost postId="1999"/></td>
-					</tr>
-					<tr>
-						<td><DiscoursePost postId="2001"/></td>
+						<td><DiscoursePost postId="13495"/></td>
 					</tr>
 					</tbody>
 				</Table>
-
-				</div>
 			</main>
 		</Layout>
 	)
