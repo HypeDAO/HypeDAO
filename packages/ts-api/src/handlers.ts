9ec419199ff457eebe9b1c88cc80eee23184ead6
EV3RETH <ev3reth@gmail.com>
2021-09-06 20:32:04 -0700

    wip

--- packages/ts-api/src/handlers.ts
+++ packages/ts-api/src/handlers.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from 'express';
 
+import { Client } from 'pg';
+import fs from 'fs';
+
+const client = new Client({
+	user: 'sgpostgres',
+	host: 'SG-NewPostgreCluster-5-pgsql-master.devservers.scalegrid.io',
+	database: 'postgres',
+	password: ‘password’',
+  port: 6432,
+	ssl: {
+		ca: fs.readFileSync('<path to CA cert file>')
+	}
+})
+client.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+});
+
 interface HelloResponse {
 	hello: string;
 }
